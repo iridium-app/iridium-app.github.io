74921cab280373d6c1870094394c34d9fff37702
Wills Martin <iridiumapp@yahoo.com>
2024-07-01 18:01:39 -0400

    Fixed some image issues

--- src/components/ItemImage.tsx
+++ src/components/ItemImage.tsx
@@ -3,6 +3,7 @@ import ItemData from "../data/ItemData";
 function ItemImage({ itemName }: { itemName: string }) {
   return (
     <img
+      className={itemName === "ITEM_NONE" || itemName === undefined ? "hidden" : ""}
       src={"/sprites/items/" + itemName + ".png"}
       title={ItemData.Dict[itemName]?.name}
     />
