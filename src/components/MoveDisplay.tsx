1fb0c6b7c4144a5b9c51a4d3b9e151e012a05410
Wills Martin <iridiumapp@yahoo.com>
2024-07-02 23:01:27 -0400

    Move description

--- src/components/MoveDisplay.tsx
+++ src/components/MoveDisplay.tsx
@@ -1,16 +1,28 @@
+import { useState } from "react";
 import MoveData from "../data/MoveData";
 import DamageCategoryImage from "./DamageCategoryImage";
 import TypeImage from "./TypeImage";
 
 function MoveDisplay({ moveName }: { moveName: string }) {
   const move = MoveData.Dict[moveName];
+  const [showFooter, setShowFooter] = useState(false);
+
+  const onClick = () => {
+    setShowFooter(!showFooter);
+  };
+
   return (
-    <div className="move-display">
-      <div className="move-display__header">{move.name}</div>
+    <div onClick={onClick} className="move-display">
+      <div className="move-display__header">
+        {move.name}
+      </div>
       <TypeImage type={move.type} />
       <DamageCategoryImage damageCategory={move.damageCategory} />
       <div>{move.pp} PP</div>
       <div>{move.basePower} BP</div>
+      <div className={showFooter ? "move-display__footer" : "hidden"}>
+        {move.description}
+      </div>
     </div>
   );
 }
